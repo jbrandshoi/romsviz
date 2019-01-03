7d6a1209205f3129f515df57efeac846d9930476
Jostein Brændshøi <josteinb@met.no>
2019-01-03 14:01:40 +0000

    added imports in init file package accessibility

---
+++ romsviz/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Easy-to-use data extraction + visualization for ROMS"""
+
+# -------------------------------------
+# Init file for the romsviz package
+#
+# Jostein Brændshøi <josteinb@met.no>
+# Norwegian Meteorological Institute
+# -------------------------------------
+
+from __future__ import absolute_import
+
+from romsviz.ncout import *
+from romsviz.outvar import *
+from romsviz.romsviz import *
